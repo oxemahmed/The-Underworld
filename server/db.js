c3f09453143a369c7bc0a1f1316ce86563a469e6
oxemahmed <oxemahmed@outlook.sa>
2026-03-15 03:37:04 +0000

    x

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
-const connectionString = 'postgresql://neondb_owner:npg_ZbFYi7RvVq5W@ep-lingering-bird-ab1zppu4-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
+const connectionString = 'postgresql://neondb_owner:npg_ZbFYi7RvVq5W@ep-lingering-bird-ab1zppu4-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=verify-full&channel_binding=require';
 
 const pool = new Pool({
   connectionString: connectionString,
   ssl: {
-    rejectUnauthorized: false // ضروري للاتصال عبر SSL
+    rejectUnauthorized: false
   }
 });
 
